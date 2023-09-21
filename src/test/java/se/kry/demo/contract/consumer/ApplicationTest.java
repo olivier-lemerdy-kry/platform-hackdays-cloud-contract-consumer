471cad1f803f7fb914a530488825b54e1c6a258c
Olivier Le Merdy <olivier.lemerdy@kry.se>
2023-09-21 09:56:17 +0200

    Random port

--- src/test/java/se/kry/demo/contract/consumer/ApplicationTest.java
+++ src/test/java/se/kry/demo/contract/consumer/ApplicationTest.java
@@ -3,25 +3,26 @@ package se.kry.demo.contract.consumer;
 import static org.assertj.core.api.Assertions.assertThat;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.cloud.contract.stubrunner.StubFinder;
 import org.springframework.cloud.contract.stubrunner.spring.AutoConfigureStubRunner;
 import org.springframework.cloud.contract.stubrunner.spring.StubRunnerProperties;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.client.RestTemplate;
 
 @SpringBootTest
 @AutoConfigureStubRunner(
-    ids = {"se.kry:platform-hackdays-cloud-contract-producer:+:stubs:9090"},
+    ids = {"se.kry:platform-hackdays-cloud-contract-producer:+:stubs"},
     stubsMode = StubRunnerProperties.StubsMode.LOCAL)
 class ApplicationTest {
 
   @Test
-  void contextLoads() {
+  void contextLoads(@Autowired StubFinder stubFinder) {
     var restTemplate = new RestTemplate();
     var payload = """
         {
@@ -32,8 +33,9 @@ class ApplicationTest {
     var httpHeaders = new HttpHeaders();
     httpHeaders.add(HttpHeaders.CONTENT_TYPE, "application/json");
 
+    var port = stubFinder.findAllRunningStubs().getPort("platform-hackdays-cloud-contract-producer");
     ResponseEntity<String> response = restTemplate.exchange(
-        "http://localhost:" + 9090 + "/fraudcheck", HttpMethod.PUT,
+        "http://localhost:" + port + "/fraudcheck", HttpMethod.PUT,
         new HttpEntity<>(payload, httpHeaders), String.class);
 
     assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
